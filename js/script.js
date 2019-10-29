5613f8cd83057fa0990f162fd5be13d46f7f373a
Kervintz Noel <10268341+kervcode@users.noreply.github.com>
2019-10-28 22:01:21 -0400

    showpage function templates

--- js/script.js
+++ js/script.js
@@ -16,35 +16,45 @@ FSJS project 2 - List Filter and Pagination
    will only be used inside of a function, then it can be locally 
    scoped to that function.
 ***/
+// grab all the the student list in the HTML DOM
+
+addEventListener('DOMContentLoaded', () => {
+   const studentList = document.querySelectorAll('.student-item');
+   const studentPerPage = 10;
+
+   /*** 
+      Create the `showPage` function to hide all of the items in the 
+      list except for the ten you want to show.
+
+      Pro Tips: 
+      - Keep in mind that with a list of 54 students, the last page 
+         will only display four.
+      - Remember that the first student has an index of 0.
+      - Remember that a function `parameter` goes in the parens when 
+         you initially define the function, and it acts as a variable 
+         or a placeholder to represent the actual function `argument` 
+         that will be passed into the parens later when you call or 
+         "invoke" the function 
+   ***/
+
+   // create function that hides all element of the student list items
+   function showPage(list, page) {
+      const startIndex = (page * studentPerPage) - studentPerPage;
+      // end index
+      const endIndex = page * studentPerPage;
+  
+   }
+   showPage();
+
+   /*** 
+      Create the `appendPageLinks function` to generate, append, and add 
+      functionality to the pagination buttons.
+   ***/
 
 
 
 
-/*** 
-   Create the `showPage` function to hide all of the items in the 
-   list except for the ten you want to show.
 
-   Pro Tips: 
-     - Keep in mind that with a list of 54 students, the last page 
-       will only display four.
-     - Remember that the first student has an index of 0.
-     - Remember that a function `parameter` goes in the parens when 
-       you initially define the function, and it acts as a variable 
-       or a placeholder to represent the actual function `argument` 
-       that will be passed into the parens later when you call or 
-       "invoke" the function 
-***/
+   // Remember to delete the comments that came with this file, and replace them with your own code comments.
 
-
-
-
-/*** 
-   Create the `appendPageLinks function` to generate, append, and add 
-   functionality to the pagination buttons.
-***/
-
-
-
-
-
-// Remember to delete the comments that came with this file, and replace them with your own code comments.
+});
